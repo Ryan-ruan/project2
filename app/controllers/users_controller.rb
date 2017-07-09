2e5daaee417768116f4dd56c71b805df272cc7a1
Ryan-ruan <ryan@ss-MacBook-Air.local>
2017-07-09 16:14:54 +1000

    cloudinary function works

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-before_action :check_if_logged_in, only: [:edit, :update, :destroy]
+before_action :check_if_logged_in, only: [ :edit, :update, :destroy]
 before_action :check_if_admin, only: [:index]
 before_action :get_user, only: [:show, :edit, :update]
 
@@ -16,11 +16,32 @@ before_action :get_user, only: [:show, :edit, :update]
   end
 
   def new
+    if @current_user.present?
+      redirect_to user_path(@current_user)
+    end
     @user = User.new
   end
 
   def create
     @user = User.new user_params
+
+    if params[:file].present?
+     # Then call Cloudinary's upload method, passing in the file in params
+     req = Cloudinary::Uploader.upload(params[:file])
+   # Using the public_id allows us to use Cloudinary's powerful image transformation methods.
+     @user.avatar = req["public_id"]
+    end
+
+    @user.save
+
+
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user.id)
+    else
+      render :new
+    end
+
   end
 
   def edit
@@ -30,6 +51,12 @@ before_action :get_user, only: [:show, :edit, :update]
   def update
 
     @user = @current_user
+
+    if params[:file].present?
+      req = Cloudinary::Uploader.upload params[:file]
+      @user.profile_image= req['public_id']
+
+    end
     @user.update user_params
 
     redirect_to user_path(params["id"])
