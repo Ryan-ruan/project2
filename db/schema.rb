7b13fd43dcfb080b5391d39df694ae680a1e1e9a
Lingxiao Wang <xwangl213@gmail.com>
2017-07-12 17:15:37 +1000

    reseed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170709040408) do
+ActiveRecord::Schema.define(version: 20170712052437) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,8 +28,9 @@ ActiveRecord::Schema.define(version: 20170709040408) do
   create_table "posts", force: :cascade do |t|
     t.text     "image"
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "like_counts"
   end
 
   create_table "users", force: :cascade do |t|
