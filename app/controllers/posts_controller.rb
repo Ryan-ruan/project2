ddb69cb732a797902b4513414b3000671b1dffd9
Lingxiao Wang <xwangl213@gmail.com>
2017-07-13 08:55:07 +1000

    merge fixed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,6 +22,7 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all
+    
     respond_to do |format|
       format.html {}
       format.json {  render json: @posts }
