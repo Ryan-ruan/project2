57bf0c34de71a4432270a4bf35026105d42f48b7
Jonathan See <mannysee@gmail.com>
2017-07-12 17:46:02 +1000

    Luke added face recognition - work in progress

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -24,6 +24,39 @@ $(document).ready(function(){
     // attach the camera(live preview) onto a div '#my_camera'
     Webcam.attach( '#my_camera' );
 
+    // Need to set an ID for the video element for trackingjs
+    $('video').attr('id', 'webcam_video');
+
+    // var $testcanvas = $('<canvas id="track-canvas" width="640" height="480">');
+    // $( '#my_camera' ).prepend($testcanvas);
+
+    var trackCanvas = document.getElementById('track-canvas');
+    console.log('canvas:', trackCanvas);
+    var context = trackCanvas.getContext('2d');
+
+
+    ///////////// start tracking code
+
+    var tracker = new tracking.ObjectTracker('face');
+    //  tracker.setInitialScale(4);
+     tracker.setStepSize(2);
+     tracker.setEdgesDensity(0.1);
+     tracking.track('#webcam_video', tracker, { camera: true });
+     tracker.on('track', function(event) {
+       context.clearRect(0, 0, trackCanvas.width, trackCanvas.height);
+       event.data.forEach(function(rect) {
+         context.strokeStyle = '#a64ceb';
+         context.strokeRect(trackCanvas.width - rect.x - rect.width, rect.y, rect.width, rect.height);
+         context.font = '11px Helvetica';
+         context.fillStyle = "#fff";
+         context.fillText('x: ' + (trackCanvas.width - rect.x - rect.width) + 'px', rect.x + rect.width + 5, rect.y + 11);
+         context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
+       });
+     });
+
+
+    ////////////////////// end tracking code
+
     var canvas = this.__canvas = new fabric.Canvas('my_canvas', {
       width: 640,
       height: 480,
