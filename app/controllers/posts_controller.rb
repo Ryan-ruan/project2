d1a5cadc00dd0e480ce2ece389ef83842d02a243
Unicar <Unicar@Macintosh.local>
2017-07-13 10:51:44 +1000

    upvote ajax

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   # before_action :redirect_to_https, only: :new
   before_action :check_if_logged_in, only: [:new, :create, :destroy]
+  before_action :find_post, only: [:show, :destroy, :like]
 
   def new
     @post = Post.new
@@ -22,7 +23,7 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all
-    
+
     respond_to do |format|
       format.html {}
       format.json {  render json: @posts }
@@ -46,9 +47,22 @@ class PostsController < ApplicationController
     redirect_to user_path(@current_user)
   end
 
+  # acts_as_votable
   def upvote
     @post = Post.find params['id']
     @post.upvote_from @current_user
-    redirect_to posts_path
+    # redirect_to posts_path
+
+    respond_to do |format|
+      format.html { redirect_to :back}
+      format.js {render layout: false}
+    end
   end
+
+  private
+
+  def find_post
+    @post = Post.find params['id']
+  end
+
 end
