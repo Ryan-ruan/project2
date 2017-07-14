563a55e26f0054c34aa035bd9c36696463ca9617
Lingxiao Wang <xwangl213@gmail.com>
2017-07-14 17:15:35 +1000

    try to fix assets on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
 
   # set serve static assets to true, so assets images can be accessed when deployed on heroku
   # ======== Lingxiao Jul 14, 2017 ================
-  config.serve_static_assets = true
+  # config.serve_static_assets = true
 
 
   # Code is not reloaded between requests.
@@ -21,14 +21,14 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = true #ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
